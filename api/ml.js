1d63f5a799559f198babe2959b5aa501eef40ade
marcola93 <30500704+marcola93@users.noreply.github.com>
2026-03-28 01:22:17 -0300

    Update ml.js

--- api/ml.js
+++ api/ml.js
@@ -9,7 +9,8 @@ function parseCookies(cookieHeader) {
 }
 
 module.exports = async (req, res) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+ res.setHeader('Access-Control-Allow-Origin', 'https://garimpeiro-git-main-marcola93s-projects.vercel.app');
+res.setHeader('Access-Control-Allow-Credentials', 'true');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
